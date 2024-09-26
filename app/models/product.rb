91e15d85fd747b5f89ad7408435990d349cd786d
Rodrigo V Hassel <rodri.hassel@gmail.com>
2024-09-26 08:03:35 -0300

    Adds product model along with some validations and associations

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Product < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :title, :user_id, presence: true
+  validates :price,
+            numericality: { greater_than_or_equal_to: 0 },
+            presence: true
+end
